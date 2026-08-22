198d40004ac4c9f593c9daa350de9953a3e85911
Alaa91H <1.43001551e+08+Alaa91H@users.noreply.github.com>
2026-08-22 14:27:19 +0000

    fix: declare paired Wear companion

--- scripts/verify_iot_integration.py
+++ scripts/verify_iot_integration.py
@@ -27,6 +27,8 @@ REQUIRED_SNIPPETS = {
     "wear/src/main/AndroidManifest.xml": [
         "android.hardware.type.watch",
         "com.google.android.gms.wearable.BIND_LISTENER",
+        "com.google.android.wearable.standalone",
+        'android:value="false"',
     ],
     "core/core-common/src/main/java/org/muslim/app/core/common/wear/WearSyncContract.kt": [
         "DATA_PATH",
