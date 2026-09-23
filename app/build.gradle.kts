2f8c2032744e0013646da86451ecd3a29f7ea8ee
Alaa <alahus2591@gmail.com>
2026-09-24 01:37:41 +0200

    merge: apply Family Life module dependency on main

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -233,6 +233,7 @@ dependencies {
     implementation(libs.androidx.lifecycle.runtime.compose)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.fragment.ktx)
     implementation(libs.androidx.window)
     implementation(libs.androidx.media)
     implementation(libs.google.play.services.wearable)
