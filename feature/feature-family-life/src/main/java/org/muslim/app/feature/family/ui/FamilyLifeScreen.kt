75f231240e0c81593edd31bf846c08e2a59718d8
Alaa <alahus2591@gmail.com>
2026-09-23 22:16:16 +0200

    feat(family): confirm article copy action

--- feature/feature-family-life/src/main/java/org/muslim/app/feature/family/ui/FamilyLifeScreen.kt
+++ feature/feature-family-life/src/main/java/org/muslim/app/feature/family/ui/FamilyLifeScreen.kt
@@ -4,6 +4,7 @@ import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
 import android.content.Intent
+import android.widget.Toast
 import androidx.core.net.toUri
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -861,6 +862,11 @@ private fun copyFamilyArticle(
             FamilyArticleTextFormatter.format(article, isArabic),
         ),
     )
+    Toast.makeText(
+        context,
+        context.getString(R.string.family_article_copied),
+        Toast.LENGTH_SHORT,
+    ).show()
 }
 
 private fun shareFamilyArticle(
