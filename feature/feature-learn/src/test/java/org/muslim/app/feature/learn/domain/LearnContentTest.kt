0ded3434b3fa3afc7f916a6fd57b4395be26e8f6
Alaa <alahus2591@gmail.com>
2026-09-24 14:22:59 +0200

    test(learn): require Sunnah and Seerah in major coverage

--- feature/feature-learn/src/test/java/org/muslim/app/feature/learn/domain/LearnContentTest.kt
+++ feature/feature-learn/src/test/java/org/muslim/app/feature/learn/domain/LearnContentTest.kt
@@ -144,6 +144,8 @@ class LearnContentTest {
             "salah", "salah_arkan", "salah_times", "adhan", "shurut", "nullifiers", "rawatib", "rakats", "special",
             "fasting", "zakat", "funeral",
             "quran_intro", "quran_understanding", "tajweed_intro", "tajweed_practice",
+            "sunnah_intro", "hadith_understanding", "hadith_library_guide",
+            "seerah_method", "seerah_hijrah", "seerah_major_events", "seerah_character_legacy",
             "madhhab",
         )
         LearnContent.topics.forEach { topic ->
