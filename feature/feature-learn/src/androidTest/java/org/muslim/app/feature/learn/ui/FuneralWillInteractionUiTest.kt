25db67be406d38b8c31c7c27047e665e371796bb
Alaa <alahus2591@gmail.com>
2026-09-24 03:33:30 +0200

    fix: remove obsolete Compose test assertion import

--- feature/feature-learn/src/androidTest/java/org/muslim/app/feature/learn/ui/FuneralWillInteractionUiTest.kt
+++ feature/feature-learn/src/androidTest/java/org/muslim/app/feature/learn/ui/FuneralWillInteractionUiTest.kt
@@ -11,7 +11,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.platform.testTag
-import androidx.compose.ui.test.assertDoesNotExist
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
