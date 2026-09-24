e181af2ae838788aa5e427e27373cb5c7421b9a0
Alaa <alahus2591@gmail.com>
2026-09-24 12:16:20 +0200

    ui: clean bookmark design token imports

--- feature/feature-quran/src/main/java/org/muslim/app/feature/quran/ui/BookmarksScreen.kt
+++ feature/feature-quran/src/main/java/org/muslim/app/feature/quran/ui/BookmarksScreen.kt
@@ -24,7 +24,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import org.muslim.app.core.designsystem.IslamicSpacing
